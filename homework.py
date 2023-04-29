90861053a891ef4adf479dd8fd03d04ec5cff021
Kirill Shirokov <kirillshirokovn@yandex.ru>
2023-04-29 17:55:15 +0300

    check_token

--- homework.py
+++ homework.py
@@ -1,15 +1,28 @@
-...
+import logging
+import os
+import time
+import sys
+from http import HTTPStatus
+
+import requests
+import telegram
+from dotenv import load_dotenv
+from logging import StreamHandler
+
+
 
 load_dotenv()
 
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+
+
 
 
 HOMEWORK_VERDICTS = {
@@ -19,33 +32,51 @@ HOMEWORK_VERDICTS = {
 }
 
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = StreamHandler(stream=sys.stdout)
+logger.addHandler(handler)
+formatter = logging.Formatter(
+    '%(asctime)s, %(levelname)s, %(message)s'
+)
+handler.setFormatter(formatter)
+
+
+class WarningMessage(Exception):
+    pass
+
+
 def check_tokens():
-    ...
+    """Проверяется доступность переменных окружения."""
+    return all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
 
 
 def send_message(bot, message):
-    ...
-
+    """Отправляется сообщение в телеграм чат."""
+    try:
+        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+        logger.info(f'Сообщение {message} отправлено')
+    except Exception as error:
+        raise WarningMessage(
+            'Ошибка при отправлении сообщения'
+        )
 
 def get_api_answer(timestamp):
-    ...
+    """Делается запрос к эндпоинту."""
 
 
 def check_response(response):
-    ...
+    """Проверяется ответ API на соответствие документации."""
 
 
 def parse_status(homework):
-    ...
+    """Извлекается статус из о конкретной д/р."""
 
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def main():
     """Основная логика работы бота."""
-
-    ...
-
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
 
